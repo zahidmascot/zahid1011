4b5df997446c4976082c4bdf17c0a243ef26e3d2
zahid1 <zahidmascot@gmail.com>
2024-11-10 06:39:29 -0800

    first commit

---
+++ loop.js
@@ -0,0 +1,46 @@
+// Assignment-2.
+
+// let num = 61;
+// while (num<100) {
+//     console.log("this is the odd number:",num);
+//     num+=2
+// } 
+
+// let number = 78;
+// while (number<100) {
+//     console.log("this is the even number:",number);
+//     number+=2
+// } 
+
+let num = 81;
+let another = 0;
+while (num<=131) {
+    console.log("this is the sum odd number:",num);
+    num+=2
+    another = another + num
+    console.log(another);
+  
+} 
+
+// let num = 206;
+// let another = 0;
+// while (num<=311) {
+//     console.log("this is the sum even number:",num);
+//     num+=2
+//     another = another + num
+//     console.log(another);
+  
+// } 
+
+
+
+
+
+
+
+
+
+
+    
+
+
